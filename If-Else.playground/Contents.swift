2a2e23b6034462040133457458928992ec993934
Antonio Caiazzo <antoniocaiazzo2003@gmail.com>
2024-03-01 13:28:58 +0100

    Lesson on If and Else

---
+++ If-Else.playground/Contents.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+let myName = "Tony"
+let myAge = 20
+let yourName = "Foo"
+let yourAge = 19
+
+if myName == "Tony" && myAge == 30 {
+    "Your name is \(myName) and age is 30"
+} else if myAge == 20 {
+    "I only guessed the age right"
+} else {
+    "Oops, ho sbagliato"
+}
+
+if (myName == "Tony" && myAge == 22) && (yourName == "Foo" || yourAge == 18) {
+    "My name is Tony and I'm 20... AND... your name is Foo or you are 19"
+} else {
+    "Hmmm, non ha funzionato bene"
+}
