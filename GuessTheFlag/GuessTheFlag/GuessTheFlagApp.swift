70dc9f8ab46dd354188bbc5f27178b45cd92c8e5
Antonio-Caiazzo <antoniocaiazzo2003@gmail.com>
2024-08-24 12:05:48 +0200

    final GuessTheFlag app

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Antonio Caiazzo on 19/08/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
