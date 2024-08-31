2a7afd3844a70f53ab94688098a25c18089b3b10
Antonio-Caiazzo <antoniocaiazzo2003@gmail.com>
2024-08-31 10:08:18 +0200

    finish challenge

---
+++ Ropascis/Ropascis/RopascisApp.swift
@@ -0,0 +1,17 @@
+//
+//  RopascisApp.swift
+//  Ropascis
+//
+//  Created by Antonio Caiazzo on 28/08/24.
+//
+
+import SwiftUI
+
+@main
+struct RopascisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
