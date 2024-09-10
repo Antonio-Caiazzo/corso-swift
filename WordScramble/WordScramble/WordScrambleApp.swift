efc6394c955ae3623dfad0271d89f8991202d4f3
Antonio-Caiazzo <antoniocaiazzo2003@gmail.com>
2024-09-10 18:56:38 +0200

    day 31

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Antonio Caiazzo on 09/09/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
